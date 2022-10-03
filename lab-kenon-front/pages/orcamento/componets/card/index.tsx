52b40246f4e054c8be960dd36426c6ea5ce86324
Lucas Farias <lucasfariaskate@gmail.com>
2022-10-03 14:58:14 -0300

    add loading in cards

--- lab-kenon-front/pages/orcamento/componets/card/index.tsx
+++ lab-kenon-front/pages/orcamento/componets/card/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Flex, Heading, Image, Skeleton, Text, VStack } from "@chakra-ui/react";
 
 export interface ICard {
   title?: string;
@@ -6,45 +6,57 @@ export interface ICard {
   tip?: string;
   img?: any;
   content?: any;
+  loading?: boolean;
 }
 
 const Card = (props: ICard) => (
-  <Flex
-    direction="column"
-    maxW="450px"
-    maxH="500px"
-    h="100%"
-    borderRadius="8px"
-    bg="gray.100"
-    color="black"
-    _dark={{
-      bg: "gray.600",
-      color: "white",
-    }}
-    cursor={"pointer"}
-    onClick={() => {
-      alert(props.title);
-    }}
-  >
-    <VStack
-      p="16px"
-      spacing="16px"
-      align="flex-start"
-      maxW="450px"
-      minW="200px"
-      h="100%"
-      w="100%"
-      overflow="auto"
-    >
-      {props?.img && (
-        <Flex justifyContent="center" justifyItems="center">
-          <Image display="flex" src={props.img} alt={props.img} w="30%" />
-        </Flex>
-      )}
-      <Heading fontSize={["md", "2xl", "2xl", "2xl"]}>{props.title}</Heading>
-      <Text overflowY="auto">{props.content}</Text>
-    </VStack>
-  </Flex>
+  <>
+    {!props.loading ? (
+      <Flex
+        direction="column"
+        maxW="450px"
+        maxH="500px"
+        h="100%"
+        borderRadius="8px"
+        bg="gray.100"
+        color="black"
+        _dark={{
+          bg: "gray.600",
+          color: "white",
+        }}
+        cursor={"pointer"}
+        onClick={() => {
+          alert(props.title);
+        }}
+      >
+        <VStack
+          p="20px"
+          spacing="16px"
+          align="flex-start"
+          maxW="450px"
+          minW="200px"
+          h="100%"
+          w="100%"
+          overflow="auto"
+        >
+          {props?.img && (
+            <Flex justifyContent="center" justifyItems="center">
+              <Image display="flex" src={props.img} alt={props.img} w="30%" />
+            </Flex>
+          )}
+
+          <Flex justifyItems="center" justifyContent="center">
+            <Heading fontSize={["md", "1xl", "1xl", "1xl"]} display="flex">
+              {props.title}
+            </Heading>
+          </Flex>
+          <Text overflowY="auto">{props.content}</Text>
+        </VStack>
+      </Flex>
+    ) : (
+      <Skeleton height="200px" width="200px" />
+    )}
+  </>
 );
 
 export default Card;
--- lab-kenon-front/pages/orcamento/index.tsx
+++ lab-kenon-front/pages/orcamento/index.tsx
@@ -1,37 +1,52 @@
 import { Flex, Grid, GridItem } from "@chakra-ui/react";
-import { memo } from "react";
+import { useEffect, useState } from "react";
+import { setTimeout } from "timers";
 import { Title } from "../../components/Title";
 import mockType from "../../public/mockType.json";
 import Card from "./componets/card";
-const Index = () => (
-  <Flex direction={"column"} gap="20px">
-    <Title title="Orcamento" size="5vw" />
-    <Flex justifyContent="center" alignItems="center">
-      <Grid
-        justifyContent={"space-around"}
-        h="300px"
-        w="500px"
-        templateColumns="repeat(4, 2fr)"
-        templateRows="repeat(2, 2fr)"
-        gap={5}
-      >
-        {mockType.map((v, i) => {
-          return (
-            <GridItem rowSpan={4} w="100%" h="100%">
-              <Card
-                key={v.id + i}
-                content={v.description}
-                description={v.description}
-                tip={v.description}
-                title={v.name}
-                img={v.icon}
-              />
-            </GridItem>
-          );
-        })}
-      </Grid>
-    </Flex>
-  </Flex>
-);
+const Index = () => {
+  const [loading, setLoading] = useState(true);
 
-export default memo(Index);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <Flex direction={"column"} gap="20px">
+      <Title title="Orcamento" size="5vw" />
+      <Flex justifyContent="center" alignItems="center">
+        <Grid
+          justifyContent={"space-around"}
+          templateColumns="repeat(4, 2fr)"
+          templateRows="repeat(2, 2fr)"
+          gap={2}
+        >
+          {mockType.map((v, i) => {
+            return (
+              <GridItem
+                rowSpan={4}
+                w="100%"
+                height="250px"
+                width="200px"
+                key={v.id + i}
+              >
+                <Card
+                  content={v.description}
+                  description={v.description}
+                  tip={v.description}
+                  title={v.name}
+                  img={v.icon}
+                  loading={loading}
+                />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Index;
